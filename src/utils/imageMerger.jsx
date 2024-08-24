4cdf1ebd5b0328cdac6c5c54e9f9642d0b68c81a
usamafayaz <usama.fayyaz157@gmail.com>
2024-08-24 23:18:37 +0500

    Ping Google before API calling

---
+++ src/utils/imageMerger.jsx
@@ -0,0 +1,49 @@
+import {NativeModules} from 'react-native';
+const {TransparifyHelper} = NativeModules;
+import RNFS from 'react-native-fs';
+
+export const mergeBackgroundAndImage = (
+  originalImage,
+  processedImage,
+  background,
+  backgroundType,
+) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const base64Image = processedImage.split(',')[1];
+      if (backgroundType === 'color') {
+        backgroundData = {type: 'color', color: background};
+      } else if (backgroundType === 'gradient') {
+        backgroundData = {
+          type: 'gradient',
+          gradient: {startColor: background[0], endColor: background[1]},
+        };
+      } else if (backgroundType === 'image') {
+        backgroundData = {type: 'image', uri: background};
+      }
+
+      const cacheDir = RNFS.CachesDirectoryPath;
+      TransparifyHelper.mergeImageWithBackground(base64Image, backgroundData)
+        .then(async res => {
+          const oldFiles = await RNFS.readDir(cacheDir);
+          let filePaths = oldFiles.map(file => file.path);
+          filePaths = filePaths.filter(path => {
+            return (
+              (path.includes('tnspym') ||
+                path.includes('rn_image_picker_lib_temp_')) &&
+              path !== res &&
+              path !== originalImage
+            );
+          });
+          await Promise.all(filePaths.map(path => RNFS.unlink(path)));
+          resolve(res);
+        })
+        .catch(error => {
+          console.log('what is the error during merge ====>', error);
+          reject(error);
+        });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
--- src/utils/removeBackgroundAPI.jsx
+++ src/utils/removeBackgroundAPI.jsx
@@ -1,6 +1,38 @@
 import {ToastAndroid} from 'react-native';
 import apiUrl from '../config/apiUrl';
+
+const checkInternetConnection = async (timeout = 5000) => {
+  const url = 'https://www.google.com';
+  const controller = new AbortController();
+  const timeoutId = setTimeout(() => controller.abort(), timeout);
+
+  try {
+    const response = await fetch(url, {
+      method: 'HEAD',
+      cache: 'no-cache',
+      signal: controller.signal,
+    });
+    console.log('Response from google:', response.ok);
+
+    clearTimeout(timeoutId);
+    return response.ok;
+  } catch (error) {
+    return false;
+  }
+};
+
 export const removeBackground = async (uri, navigation) => {
+  const isConnected = await checkInternetConnection();
+
+  if (!isConnected) {
+    navigation.goBack();
+    ToastAndroid.show(
+      'Please check your internet connection and try again.',
+      ToastAndroid.SHORT,
+    );
+    return;
+  }
+
   const formData = new FormData();
   formData.append('image', {
     uri: uri,
@@ -31,7 +63,7 @@ export const removeBackground = async (uri, navigation) => {
   } catch (error) {
     navigation.goBack();
     ToastAndroid.show(
-      'Please check your internet connection and try again.',
+      'An error occurred while processing your request. Please try again.',
       ToastAndroid.SHORT,
     );
   }
